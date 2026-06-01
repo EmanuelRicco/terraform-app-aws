0560ca2ac51f5f1eec8ea12cf3f5356033499980
EmanuelRicco <emanuelricco.infra@gmail.com>
2026-06-01 09:50:17 -0300

    Ajustar formatação e corrigir a definição de user_data_base64 no recurso aws_instance

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -7,12 +7,11 @@ resource "aws_instance" "vm_app" {
   ami           = "ami-0533a139e02b00be4"
   instance_type = "t2.micro"
   key_name      = aws_key_pair.app_key.key_name
-
-  subnet_id              = aws_subnet.subnet_app.id
+  subnet_id     = aws_subnet.subnet_app.id
   vpc_security_group_ids = [aws_security_group.sg_app.id]
+  availability_zone = "sa-east-1a"
 
-
-  user_data = base64encode(templatefile("${path.module}/script/docker_install.sh", {
+  user_data_base64 = base64encode(templatefile("${path.module}/script/docker_install.sh", {
     app_py       = file("${path.module}/../app/app.py")
     dockerfile   = file("${path.module}/../app/Dockerfile")
     requirements = file("${path.module}/../app/requirements.txt")
