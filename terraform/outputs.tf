5b9d621d4543c84336b3d6d174586ff8e258c2ee
Emanuel Ricco de Macedo <emanuelricco.infra@gmail.com>
2026-06-01 09:24:39 -0300

    Remove SSH connection output
    Removed SSH connection output from outputs.tf.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "conexao_ssh" {
-  description = "Comando para acessar a instancia via SSH"
-  value       = "ssh -i ~/.ssh/id_ed25519 ubuntu@${aws_instance.vm_app.public_ip}"
-}
 
 output "app_url" {
   description = "URL para acessar a aplicacao no navegador"
